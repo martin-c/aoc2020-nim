c649af152ec9df49eee50bab63012667ca773a75
martin <martin.clemons@mac.com>
2020-12-16 20:55:51 +0100

    add day 16 part 2 🎫🎫🎟

--- 16/aoc2020_16_p2.nim
+++ 16/aoc2020_16_p2.nim
@@ -18,13 +18,13 @@ import streams
 import strformat
 import strscans
 import strutils
-#import sugar
+import sugar
 import tables
 
 
 type
     Rule = object
-        test: proc(v: int): bool
+        test: proc(v: int): bool {.noSideEffect.}
         label: string
 
 
@@ -72,7 +72,7 @@ func parseRule(ruledef: string): Rule =
     # class: 1-3 or 5-7
     # debugEcho ruledef
     assert ruledef.scanf("$+: $i-$i or $i-$i", label, min1, max1, min2, max2)
-    result.test = proc(v: int): bool =
+    result.test = proc(v: int): bool {.noSideEffect.} =
         (v >= min1 and v <= max1) or (v >= min2 and v <= max2)
     result.label = label
 
@@ -81,11 +81,50 @@ func parseTicket(line: string): seq[int] =
     line.split(',').map(parseInt)
 
 
-proc errorRate(tickets: seq[seq[int]], rules: seq[Rule]): int =
-    for tkt in tickets:
-        for fld in tkt:
-            if not rules.anyIt(it.test(fld)):
-                result += fld
+func ticketValid(ticket: seq[int], rules: seq[Rule]): bool =
+    for fld in ticket:
+        if not rules.anyIt(it.test(fld)):
+            return false
+    true
+
+
+iterator fields(tickets: seq[seq[int]]): (int, seq[int]) =
+    ## transpose a sequence of tickets into a seqence of the same field from
+    ## each ticket. Iterator `i` counts up for each field offset
+    var col = newSeq[int](tickets.len)
+    for f in 0..<tickets[0].len:
+        for t in 0..<tickets.len:
+            col[t] = tickets[t][f]
+        yield (f, col)
+
+
+proc validFields(tickets: seq[seq[int]], rules: seq[Rule],
+        fmap: OrderedTableRef[string, seq[int]]) =
+    # map of valid fields where key is the field label and value is sequence of
+    # the field offset(s) which are valid
+    for rule in rules:
+        var s: seq[int]
+        for (i, field) in tickets.fields:
+            if field.all(rule.test):
+                s.add(i)
+        fmap[rule.label] = s
+
+
+proc solveFields(fmap: OrderedTableRef[string, seq[int]]) =
+    var exclude: seq[int]
+    # sort by length of sequence of field offsets
+    fmap.sort((x, y) => x[1].len - y[1].len)
+    for possible_fields in fmap.mvalues:
+        possible_fields.keepItIf(it notin exclude)
+        # if this is false there are at least two valid combinations
+        assert(possible_fields.len == 1)
+        exclude.add(possible_fields[0])
+
+
+func mergeTicketValues(fmap: OrderedTableRef[string, seq[int]], ticket: seq[int]):
+        Table[string, int] =
+    for (label, field) in fmap.pairs:
+        result[label] = ticket[field[0]]
 
 
 if isMainModule:
@@ -93,7 +132,17 @@ if isMainModule:
     let
         res = splitInput()
         rules = res[0].map(parseRule)
-        ticket = res[1].map(parseTicket)
+        ticket = res[1].map(parseTicket)[0]
         nearby_tickets = res[2].map(parseTicket)
-        error_rate = nearby_tickets.error_rate(rules)
-    echo "computed error rate: ", error_rate
+        valid_tickets = nearby_tickets.filterIt(ticketValid(it, rules))
+    echo "nearby tickets: ", nearby_tickets.len, " valid nearby tickets: ", valid_tickets.len
+    var field_map = newOrderedTable[string, seq[int]]()
+    validFields(valid_tickets, rules, field_map)
+    solveFields(field_map)
+    let ticket_map = mergeTicketValues(field_map, ticket)
+    echo ticket_map
+    var product = 1
+    for (label, val) in ticket_map.pairs:
+        if label.split(" ")[0] == "departure":
+            product *= val
+    echo "product: ", product
